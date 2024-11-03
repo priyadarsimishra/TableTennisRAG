c9f8193d8173d01ac6b392e8ca7d0980ae3763d2
Alan <alan.zhou.0@gmail.com>
2024-11-03 00:36:52 -0500

    frontend initial

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [file, setFile] = useState(null);
+    const [advice, setAdvice] = useState(null);
+
+    useEffect(() => {
+        return () => {
+            if (file && file.preview) {
+                console.log('Revoking URL:', file.preview);
+                URL.revokeObjectURL(file.preview);
+            }
+        };
+    }, [file]);
+
+    const handleFileChange = (event) => {
+        if (file && file.preview) {
+            console.log('Revoking URL:', file.preview);
+            URL.revokeObjectURL(file.preview);
+        }
+        const newFile = event.target.files[0];
+        if (newFile) {
+            newFile.preview = URL.createObjectURL(newFile);
+            console.log('Creating URL:', newFile.preview);
+            setFile(newFile);
+        }
+        setAdvice("get better");
+    };
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>Upload ping pong video (MP4)</p>
+                <input type="file" accept=".mp4" onChange={handleFileChange} />
+                {file && file.type === 'video/mp4' && (
+                    <video key={file.preview} width="320" height="240" controls>
+                        <source src={file.preview} type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                )}
+                <p>{advice}</p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
